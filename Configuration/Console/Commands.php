2f26ae03437a55de9667dd501bad615b90d7ed78
Helmut Hummel <info@helhum.io>
2018-01-03 14:14:10 +0100

    Use Symfony commands instead of command controller

--- Configuration/Console/Commands.php
+++ Configuration/Console/Commands.php
@@ -1,7 +1,12 @@
 <?php
 return [
-    'controllers' => [
-        \Helhum\Typo3ConfigHandling\Command\SettingsCommandController::class,
+    'commands' => [
+        'settings:dump' => [
+            'class' => \Helhum\TYPO3\ConfigHandling\Command\SettingsDumpCommand::class,
+        ],
+        'settings:extract' => [
+            'class' => \Helhum\TYPO3\ConfigHandling\Command\SettingsExtractCommand::class,
+        ],
     ],
     'runLevels' => [
         'helhum/typo3-config-handling:settings:*' => \Helhum\Typo3Console\Core\Booting\RunLevel::LEVEL_COMPILE,
