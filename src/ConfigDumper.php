679a192bd6de3763875c522732d566cd70001d30
Helmut Hummel <info@helhum.io>
2018-02-28 14:29:40 +0100

    [FEATURE] Add option to dump config as dynamic PHP code
    To satisfy the need to directly react e.g. on environment
    variable changes, it is required that the exported configuration
    array directly pulls environment variables out of the environment.
    
    By doing so, the exported configuration can stay the same
    (e.g. in a container), but the environment can still change
    and the Configuration will adapt.
    
    While doing so, we create PHP code for the other placeholders
    as well (global, const, conf) to be consistent.
    
    Since this approach has slightly decreased runtime performance,
    this is done optionally, thus only when users decide to use the
    introduced --code option for dumping configuration.

--- src/ConfigDumper.php
+++ src/ConfigDumper.php
@@ -22,6 +22,8 @@ namespace Helhum\TYPO3\ConfigHandling;
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
+use Helhum\ConfigLoader\Config;
+use Helhum\ConfigLoader\Processor\PlaceholderValue;
 use Symfony\Component\Yaml\Yaml;
 use TYPO3\CMS\Core\Utility\ArrayUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -70,5 +72,121 @@ EOF;
                 return $commentChar . ' ' . $line;
             }, explode(chr(10), $comment))
         );
+    }
+
+    /**
+     * Inspired by \TYPO3\CMS\Core\Utility\ArrayUtility::arrayExport
+     * but cleaned up and more powerful
+     *
+     * @param array $config
+     * @param array $referenceConfig
+     * @param array $path
+     * @return string
+     */
+    public function getPhpCode(array $config, array $referenceConfig = [], array $path = []): string
+    {
+        $lines = '[' . LF;
+        $level = count($path);
+        foreach ($config as $key => $value) {
+            // Indention
+            $lines .= str_repeat('    ', $level + 1);
+            // Integer / string keys
+            $lines .= is_int($key) ? $key . ' => ' : $this->getPhpCodeForValue($key, $referenceConfig) . ' => ';
+            if (is_array($value)) {
+                if (!empty($value)) {
+                    if ($level === 2 && $path[0] === 'EXT' && $path[1] === 'extConf') {
+                        $lines .= 'serialize(';
+                        $lines .= substr($this->getPhpCode($this->addDotsToTypoScript($value), $referenceConfig, array_merge($path, [$key])), 0, -2);
+                        $lines .= '),' . LF;
+                    } else {
+                        $lines .= $this->getPhpCode($value, $referenceConfig, array_merge($path, [$key]));
+                    }
+                } else {
+                    $lines .= '[],' . LF;
+                }
+            } elseif (is_int($value) || is_float($value)) {
+                $lines .= $value . ',' . LF;
+            } elseif ($value === null) {
+                $lines .= 'null' . ',' . LF;
+            } elseif (is_bool($value)) {
+                $lines .= $value ? 'true' : 'false';
+                $lines .= ',' . LF;
+            } elseif (is_string($value)) {
+                $lines .= $this->getPhpCodeForValue($value, $referenceConfig, $path) . ',' . LF;
+            } else {
+                throw new \RuntimeException('Objects are not supported', 1519779656);
+            }
+        }
+        $lines .= str_repeat('    ', $level) . ']' . ($level === 0 ? '' : ',' . LF);
+        return $lines;
+    }
+
+    private function isPlaceHolder($value): bool
+    {
+        return is_string($value) && preg_match(PlaceholderValue::PLACEHOLDER_PATTERN, $value);
+    }
+
+    private function getPhpCodeForValue(string $value, array $referenceConfig, array $path = []): string
+    {
+        if (!$this->isPlaceHolder($value)) {
+            return '\'' . addcslashes($value, '\\\'') . '\'';
+        }
+        $placeholderInfo = $this->extractPlaceHolder($value);
+        $phpCode = '\'' . $value . '\'';
+        switch ($placeholderInfo['type']) {
+            case 'env':
+                $phpCode = 'getenv(\'' . $placeholderInfo['accessor'] . '\')';
+                break;
+            case 'const':
+                $phpCode = 'constant(\'' . $placeholderInfo['accessor'] . '\')';
+                break;
+            case 'conf':
+                $phpCode = substr($this->getPhpCode(Config::getValue($referenceConfig, $placeholderInfo['accessor']), $referenceConfig, $path), 0, -2);
+                break;
+            case 'global':
+                $globalPath = str_getcsv($placeholderInfo['accessor'], '.');
+                $phpCode = '$GLOBALS[\'' . implode('\'][\'', $globalPath) . '\']';
+                break;
+        }
+        if ($placeholderInfo['isDirectMatch']) {
+            return $phpCode;
+        }
+
+        return '\'' . preg_replace(PlaceholderValue::PLACEHOLDER_PATTERN, '\' . ' . $phpCode . ' . \'', $value) . '\'';
+    }
+
+    private function extractPlaceHolder($value, array $types = null): array
+    {
+        if (!$this->isPlaceHolder($value)) {
+            return [];
+        }
+        preg_match(PlaceholderValue::PLACEHOLDER_PATTERN, $value, $matches);
+        if ($types !== null && !in_array($matches[1], $types, true)) {
+            return [];
+        }
+        return [
+            'placeholder' => $matches[0],
+            'type' => $matches[1],
+            'accessor' => $matches[2],
+            'isDirectMatch' => $matches[0] === $value,
+        ];
+    }
+
+    /**
+     * @param array $typoScript TypoScript configuration array
+     * @return array TypoScript configuration array without dots at the end of all keys
+     */
+    private function addDotsToTypoScript(array $typoScript): array
+    {
+        $out = [];
+        foreach ($typoScript as $key => $value) {
+            if (is_array($value)) {
+                $key .= '.';
+                $out[$key] = $this->addDotsToTypoScript($value);
+            } else {
+                $out[$key] = $value;
+            }
+        }
+        return $out;
     }
 }
