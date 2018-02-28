587d09108670a59120982134e2839d7f9db53a20
Helmut Hummel <info@helhum.io>
2018-02-28 16:10:00 +0100

    Refactor getPhpCode method to accept arbitrary values
    Instead of only accepting arrays, it now accepts
    scalar values as well, which made it possible to
    straighten code that uses it.

--- src/ConfigDumper.php
+++ src/ConfigDumper.php
@@ -23,6 +23,7 @@ namespace Helhum\TYPO3\ConfigHandling;
  ***************************************************************/
 
 use Helhum\ConfigLoader\Config;
+use Helhum\ConfigLoader\InvalidConfigurationFileException;
 use Helhum\ConfigLoader\Processor\PlaceholderValue;
 use Symfony\Component\Yaml\Yaml;
 use TYPO3\CMS\Core\Utility\ArrayUtility;
@@ -75,50 +76,49 @@ EOF;
     }
 
     /**
-     * Inspired by \TYPO3\CMS\Core\Utility\ArrayUtility::arrayExport
-     * but cleaned up and more powerful
+     * Returns a PHP representation of a value, including value with placeholder
      *
-     * @param array $config
+     * @param mixed $value
      * @param array $referenceConfig
      * @param array $path
      * @return string
      */
-    public function getPhpCode(array $config, array $referenceConfig = [], array $path = []): string
+    public function getPhpCode($value, array $referenceConfig = [], array $path = []): string
     {
-        $lines = '[' . LF;
-        $level = count($path);
-        foreach ($config as $key => $value) {
-            // Indention
-            $lines .= str_repeat('    ', $level + 1);
-            // Integer / string keys
-            $lines .= is_int($key) ? $key . ' => ' : $this->getPhpCodeForValue($key, $referenceConfig) . ' => ';
-            if (is_array($value)) {
-                if (!empty($value)) {
-                    if ($level === 2 && $path[0] === 'EXT' && $path[1] === 'extConf') {
-                        $lines .= 'serialize(';
-                        $lines .= substr($this->getPhpCode($this->addDotsToTypoScript($value), $referenceConfig, array_merge($path, [$key])), 0, -2);
-                        $lines .= '),' . LF;
-                    } else {
-                        $lines .= $this->getPhpCode($value, $referenceConfig, array_merge($path, [$key]));
-                    }
-                } else {
-                    $lines .= '[],' . LF;
-                }
-            } elseif (is_int($value) || is_float($value)) {
-                $lines .= $value . ',' . LF;
-            } elseif ($value === null) {
-                $lines .= 'null' . ',' . LF;
-            } elseif (is_bool($value)) {
-                $lines .= $value ? 'true' : 'false';
-                $lines .= ',' . LF;
-            } elseif (is_string($value)) {
-                $lines .= $this->getPhpCodeForValue($value, $referenceConfig, $path) . ',' . LF;
+        if (is_array($value)) {
+            $level = count($path);
+            if ($value === []) {
+                $code = '[]';
             } else {
-                throw new \RuntimeException('Objects are not supported', 1519779656);
+                $code = '[' . PHP_EOL;
+                foreach ($value as $key => $arrayValue) {
+                    // Indention
+                    $code .= str_repeat('    ', $level + 1);
+                    // Integer / string keys
+                    $code .= is_int($key) ? $key . ' => ' : $this->getPhpCodeForPlaceholder($key, $referenceConfig, [], true) . ' => ';
+                    if ($level === 2 && $path[0] === 'EXT' && $path[1] === 'extConf') {
+                        $code .= 'serialize(';
+                        $code .= $this->getPhpCode($this->addDotsToTypoScript($arrayValue), $referenceConfig, array_merge($path, [$key]));
+                        $code .= ')';
+                    } else {
+                        $code .= $this->getPhpCode($arrayValue, $referenceConfig, array_merge($path, [$key]));
+                    }
+                    $code .= ',' . PHP_EOL;
+                }
+                $code .= str_repeat('    ', $level) . ']';
             }
+        } elseif (is_int($value) || is_float($value)) {
+            $code = (string)$value;
+        } elseif ($value === null) {
+            $code = 'null';
+        } elseif (is_bool($value)) {
+            $code = $value ? 'true' : 'false';
+        } elseif (is_string($value)) {
+            $code = $this->getPhpCodeForPlaceholder($value, $referenceConfig, $path);
+        } else {
+            throw new \RuntimeException('Objects, closures and resources are not supported', 1519779656);
         }
-        $lines .= str_repeat('    ', $level) . ']' . ($level === 0 ? '' : ',' . LF);
-        return $lines;
+        return $code;
     }
 
     private function isPlaceHolder($value): bool
@@ -126,7 +126,7 @@ EOF;
         return is_string($value) && preg_match(PlaceholderValue::PLACEHOLDER_PATTERN, $value);
     }
 
-    private function getPhpCodeForValue(string $value, array $referenceConfig, array $path = []): string
+    private function getPhpCodeForPlaceholder(string $value, array $referenceConfig, array $path = [], bool $forKey = false): string
     {
         if (!$this->isPlaceHolder($value)) {
             return '\'' . $this->escapePhpValue($value) . '\'';
@@ -141,7 +141,8 @@ EOF;
                 $phpCode = 'constant(\'' . $this->escapePhpValue($placeholderInfo['accessor']) . '\')';
                 break;
             case 'conf':
-                $phpCode = substr($this->getPhpCode(Config::getValue($referenceConfig, $placeholderInfo['accessor']), $referenceConfig, $path), 0, -2);
+                $subConfig = Config::getValue($referenceConfig, $placeholderInfo['accessor']);
+                $phpCode = $this->getPhpCode($subConfig, $referenceConfig, $path);
                 break;
             case 'global':
                 $globalPath = str_getcsv($placeholderInfo['accessor'], '.');
