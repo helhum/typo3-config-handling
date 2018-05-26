4fb7fe5f25f1e2bb4c8c4f4fcc962c0f3906db9a
Helmut Hummel <info@helhum.io>
2018-05-27 00:25:04 +0200

    Prepare travis CI test

---
+++ src/ConfigReader/ArrayReader.php
@@ -0,0 +1,68 @@
+<?php
+declare(strict_types=1);
+namespace Helhum\TYPO3\ConfigHandling\ConfigReader;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2018 Helmut Hummel <info@helhum.io>
+ *  All rights reserved
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the text file GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use Helhum\ConfigLoader\Config;
+use Helhum\ConfigLoader\Reader\ConfigReaderInterface;
+
+class ArrayReader implements ConfigReaderInterface
+{
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * @var string
+     */
+    private $configPath;
+
+    public function __construct(array $config, string $configPath = null)
+    {
+        $this->config = $config;
+        $this->configPath = $configPath;
+    }
+
+    public function hasConfig(): bool
+    {
+        if ($this->configPath) {
+            $config = Config::getValue($this->config, $this->configPath, []);
+            return \is_array($config) && $config !== [];
+        }
+
+        return $this->config !== [];
+    }
+
+    public function readConfig(): array
+    {
+        if ($this->configPath) {
+            $config = Config::getValue($this->config, $this->configPath, []);
+            if (!\is_array($config)) {
+                throw new \RuntimeException(sprintf('Config for path "%s" is not an array', $this->configPath), 1527373063);
+            }
+            return Config::getValue($this->config, $this->configPath, []);
+        }
+
+        return $this->config;
+    }
+}
---
+++ tests/Unit/Reader/ArrayReaderTest.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+namespace Helhum\TYPO3\ConfigHandling\Tests\Unit\Reader;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2018 Helmut Hummel <info@helhum.io>
+ *  All rights reserved
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *  A copy is found in the text file GPL.txt and important notices to the license
+ *  from the author is found in LICENSE.txt distributed with these scripts.
+ *
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use Helhum\ConfigLoader\PathDoesNotExistException;
+use Helhum\TYPO3\ConfigHandling\ConfigReader\ArrayReader;
+use PHPUnit\Framework\TestCase;
+
+class ArrayReaderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function readConfigReturnsGivenArray()
+    {
+        $input = [
+            'foo' => 'bar',
+        ];
+
+        $reader = new ArrayReader($input);
+
+        $this->assertSame($input, $reader->readConfig());
+    }
+
+    /**
+     * @test
+     */
+    public function readConfigThrowsExceptionOnNotExistingPath()
+    {
+        $this->expectException(PathDoesNotExistException::class);
+        $input = [
+            'foo' => 'bar',
+        ];
+
+        $reader = new ArrayReader($input, 'bla');
+        $reader->readConfig();
+    }
+
+    /**
+     * @test
+     */
+    public function readConfigThrowsExceptionOnInvalidPath()
+    {
+        $this->expectException(\RuntimeException::class);
+        $input = [
+            'foo' => 'bar',
+        ];
+
+        $reader = new ArrayReader($input, 'foo');
+        $reader->readConfig();
+    }
+
+    /**
+     * @test
+     */
+    public function readConfigReturnsGivenArrayPath()
+    {
+        $input = [
+            'foo' => [
+                'bar' => 'baz',
+            ],
+        ];
+
+        $reader = new ArrayReader($input, 'foo');
+
+        $this->assertSame(['bar' => 'baz'], $reader->readConfig());
+    }
+}
