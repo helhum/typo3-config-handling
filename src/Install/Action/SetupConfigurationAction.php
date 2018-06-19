4168543b22cb7092c2428739d9919be9497f43b2
Helmut Hummel <info@helhum.io>
2018-06-19 13:57:32 +0200

    Move setup action to proper namespace

--- src/Install/Action/SetupConfigurationAction.php
+++ src/Install/Action/SetupConfigurationAction.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-namespace Helhum\TYPO3\ConfigHandling\Composer;
+namespace Helhum\TYPO3\ConfigHandling\Install\Action;
 
 /***************************************************************
  *  Copyright notice
