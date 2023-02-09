ef714671627725656a90eae115fdc0dc84febd5b
Sebastian Michaelsen <sebastian@michaelsen.io>
2023-02-09 10:51:47 +0100

    [BUGFIX] Fix cache flush if caching is enabled via `TYPO3_CONFIG_HANDLING_CACHE`

--- src/ConfigLoader.php
+++ src/ConfigLoader.php
@@ -133,7 +133,7 @@ EOF;
 
     public function flushCache(): void
     {
-        if (!$this->isProduction) {
+        if (!$this->isProduction && !getenv('TYPO3_CONFIG_HANDLING_CACHE')) {
             return;
         }
         $cacheFilePattern = str_replace($this->getCacheIdentifier(), '*', $this->getCacheFile());
