92d51407235018ad38ba0172177d2e98e7ce0325
Helmut Hummel <typo3@helhum.io>
2025-04-23 16:50:23 +0200

    [FEATURE] Allow disabling config handling by env var
    To allow testing in non composer testing framework mode
    the class_alias is now skipped when TYPO3_TESTING env var is set

--- res/php/autoload-include.php
+++ res/php/autoload-include.php
@@ -1,2 +1,4 @@
 <?php
-class_alias(\Helhum\TYPO3\ConfigHandling\Xclass\ConfigurationManager::class, \TYPO3\CMS\Core\Configuration\ConfigurationManager::class);
+if (!isset($_ENV['TYPO3_TESTING']) && getenv('TYPO3_TESTING') === false) {
+    class_alias(\Helhum\TYPO3\ConfigHandling\Xclass\ConfigurationManager::class, \TYPO3\CMS\Core\Configuration\ConfigurationManager::class);
+}
